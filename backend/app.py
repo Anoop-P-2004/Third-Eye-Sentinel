5b80efab34c25bacf4c787da80e0f8a128c0b556
Anoop P <anoop171215@gmail.com>
2025-03-12 20:05:52 +0530

    implements token verification and backend for admin page

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
 import os
 from dotenv import load_dotenv
 import jwt
-from flask import Flask,request,jsonify,render_template
+from flask import Flask,request,jsonify,render_template,g
 from flask_bcrypt import Bcrypt
 import firebase_admin
 from firebase_admin import credentials,firestore,db
+from functools import wraps
 
 load_dotenv()
 
@@ -16,9 +17,23 @@ app=Flask(__name__,template_folder=TEMPLATE_DIR,static_folder=STATIC_DIR)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') #secret key is stored in .env file
 bcrypt=Bcrypt(app)
 
-cred = credentials.Certificate('Third-Eye-Sentinel/backend/service_key.json')
+cred = credentials.Certificate('backend/service_key.json')
 firebase_admin.initialize_app(cred)
 db=firestore.client()
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('Authorization')
+        if not token:
+            return render_template("error.html",msg="Please login first"), 401
+        try:
+            decoded_token = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            g.user = decoded_token
+        except jwt.InvalidTokenError:
+            return render_template("error.html",msg="invalid token"), 401
+        return f(*args, **kwargs)
+    return decorated
 
 def hash_password(password):
     return bcrypt.generate_password_hash(password).decode('utf-8')
@@ -30,8 +45,28 @@ def check_password(hashed_password,plain_password):
 def index():
     return render_template("login.html")
 
-@app.route("/signup",methods=['POST'])
+@app.route("/signup")
 def signup():
+    return render_template("signup.html")
+
+@app.route("/admin")
+@token_required
+def admin():
+    if g.user["role"]=="admin":
+        return render_template("admin.html")
+    else:
+        return render_template("error.html",msg="Only admins can view this..")
+
+@app.route("/demo")
+@token_required
+def demo():
+    if g.user["role"]=="admin":
+        return render_template("demo.html")
+    else:
+        return render_template("error.html",msg="Only admins can view this..")
+
+@app.route("/signup/submit",methods=['POST'])
+def signup_submit():
     data=request.get_json()
     username=data["username"]
     password=data["password"]
@@ -49,6 +84,8 @@ def signup():
         }
         db.collection("users").document(username).set(user_data)
         return jsonify({"flag":0,"message":"Account created successfully..."})
+
+
 
 @app.route("/login",methods=['POST'])
 def login():
@@ -70,6 +107,59 @@ def login():
     else:
         return jsonify({"flag":1,"message":"Invalid credentials..."})
 
+@app.route("/manage_user")
+@token_required
+def manage_user():
+    if g.user["role"]=="admin":
+        docs=db.collection("users").stream()
+        users=[{"username":doc.id,"role":doc.to_dict().get('role')} for doc in docs]
+        return render_template("manage_user.html",users=users)
+    else:
+        return render_template("error.html",msg="Only admins can view this..")
+
+@app.route("/manage_cameras")
+@token_required
+def manage_cameras():
+    if g.user["role"]=="admin":
+        return render_template("manage_cameras.html")
+    else:
+        return render_template("error.html",msg="Only admins can view this..")
+
+@app.route("/manage_admin")
+@token_required
+def manage_admin():
+    if g.user["role"]=="admin":
+        return render_template("manage_admin.html")
+    else:
+        return render_template("error.html",msg="Only admins can view this..")
+
+
+@app.route("/view_report")
+@token_required
+def view_report():
+        return render_template("view_report.html")
+
+@app.route("/manage_user/delete_user")
+@token_required
+def delete_user():
+    if g.user["role"]=="admin":
+        username=request.args.get("username")
+        db.collection("users").document(username).delete()
+        return jsonify({"flag":0,"message":"Deleted User Successfully..."})
+    else:
+        return render_template("error.html",msg="Only admins can view this..")
+    
+
+@app.route("/manage_user/make_admin")
+@token_required
+def make_admin():
+    if g.user["role"]=="admin":
+        username=request.args.get("username")
+        db.collection("users").document(username).update({"role":"admin"})
+        return jsonify({"flag":0})
+    else:
+        return render_template("error.html",msg="Only admins can view this..")
+    
 
 if __name__=="__main__":
     app.run(debug=True)
