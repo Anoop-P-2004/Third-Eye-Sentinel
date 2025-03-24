78e3f4d0f592c87d70cb1f57a8dfed0e62304721
Anoop-P-2004 <anoop171215@gmail.com>
2025-03-24 22:23:24 +0530

    Delete backend/tempCodeRunnerFile.py

--- backend/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-@app.route("/manage_user")
-@token_required
-def manage_user():
-    if g.user["role"]=="admin":
-        docs=db.collection("users").stream()
-        users=[{"username":doc.id,"role":doc.to_dict().get('role')} for doc in docs]
-        return render_template("manage_user.html",users=users)
-    else:
-        return render_template("error.html",msg="Only admins can view this..")
